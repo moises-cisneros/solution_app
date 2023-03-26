84e498e02fcaad0d884b5bf64cccca378868f69d
moises-cisneros <moisescisnerosdl@gmail.com>
2023-03-26 03:50:34 -0400

    Diseñando la pantalla login y dashboard

---
+++ lib/app/screens/home_dashboard.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'login.dart';
+
+class ScreenDashboard extends StatefulWidget {
+  const ScreenDashboard({super.key});
+
+  @override
+  State<ScreenDashboard> createState() => _ScreenDashboardState();
+}
+
+class _ScreenDashboardState extends State<ScreenDashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(actions: <Widget>[
+        IconButton(
+            onPressed: () {
+              Navigator.push(context,
+                  MaterialPageRoute(builder: (context) => const LoginPage()));
+            },
+            icon: const Icon(Icons.exit_to_app)),
+      ]),
+      drawer: Drawer(
+        child: ListView(
+          children: [
+            const UserAccountsDrawerHeader(
+              accountName: Text('Nombre del usuario'),
+              accountEmail: Text('correo@example.com'),
+              currentAccountPicture: CircleAvatar(
+                  //backgroundImage: AssetImage('assets/images/profile_image.jpg'),
+                  ),
+              margin: EdgeInsets.zero,
+            ),
+
+            ListTile(
+              leading: const Icon(Icons.edit),
+              title: const Text('Editar perfil'),
+              onTap: () {},
+            ),
+            const Divider(
+              height: 18.0,
+              color: Colors.white,
+            ),
+
+            //Opciones de las pantallas
+            ListTile(
+              leading: const Icon(Icons.warning),
+              title: const Text('Preventive measures'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.sick_outlined),
+              title: const Text('Dengue symptoms'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.receipt_long_outlined),
+              title: const Text('Diagnostic test'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.video_collection),
+              title: const Text('Informative videos'),
+              onTap: () {},
+            ),
+            ListTile(
+              leading: const Icon(Icons.open_in_new),
+              title: const Text('References'),
+              onTap: () {},
+            ),
+
+            const Divider(
+              height: 25.0,
+              color: Colors.white,
+            ),
+
+            Column(
+              children: [
+                ElevatedButton(
+                    onPressed: () {
+                      Navigator.of(context).pop();
+                    },
+                    child: const Text("CLOSE")),
+              ],
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/app/screens/login.dart
@@ -0,0 +1,177 @@
+import 'package:flutter/material.dart';
+import 'package:solution/app/screens/home_dashboard.dart';
+
+class LoginPage extends StatefulWidget {
+  const LoginPage({Key? key}) : super(key: key);
+
+  @override
+  // ignore: library_private_types_in_public_api
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _rememberPassword = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        //Diseño de la flecha de retroceder
+        elevation: 0.0,
+        centerTitle: true,
+        leading: const Icon(
+          Icons.turn_left,
+          color: Colors.black,
+          size: 24.0,
+        ),
+        backgroundColor: Colors.white,
+      ),
+      body: ListView(
+        padding: const EdgeInsets.all(40.0),
+        children: <Widget>[
+          //Titulo
+          Container(
+            alignment: Alignment.center,
+            padding: const EdgeInsets.all(20.0),
+            child: const Text(
+              'LOGIN',
+              style: TextStyle(color: Colors.blue),
+            ),
+          ),
+
+          //Contenedor del usuario
+          TextField(
+            enableInteractiveSelection: false,
+            autofocus: true,
+            textCapitalization: TextCapitalization.characters,
+            decoration: InputDecoration(
+              icon: const Icon(Icons.account_circle),
+              hintText: 'User name',
+              labelText: 'USER',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+          ),
+
+          const Divider(
+            height: 18.0,
+            color: Colors.white,
+          ),
+
+          //Contenedor de la contraseña
+          TextField(
+            enableInteractiveSelection: false,
+            obscureText: true,
+            autofocus: true,
+            textCapitalization: TextCapitalization.characters,
+            decoration: InputDecoration(
+              icon: const Icon(Icons.lock_outline),
+              hintText: 'Password',
+              labelText: 'PASSWORD',
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
+            ),
+          ),
+
+          //Diseño del remember password
+          Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Checkbox(
+                value: _rememberPassword,
+                onChanged: (bool? value) {
+                  setState(() {
+                    _rememberPassword = value ?? false;
+                  });
+                },
+              ),
+              const Text('Remember password'),
+            ],
+          ),
+
+          const Divider(
+            height: 18.0,
+            color: Colors.white,
+          ),
+
+          //Boton de ingreso
+          Column(children: [
+            ElevatedButton(
+                onPressed: () {
+                  //Navegacion hacia el screen dashboard
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ScreenDashboard()));
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: const Text(
+                  "INGRESAR",
+                ))
+          ]),
+
+          const Divider(
+            height: 18.0,
+            color: Colors.white,
+          ),
+
+          //Data adicional
+          Container(
+              alignment: Alignment.center,
+              child: const Text("You do not have an account?")),
+          const Divider(
+            height: 18.0,
+            color: Colors.white,
+          ),
+
+          //Boton de registro
+          Column(children: [
+            ElevatedButton(
+                onPressed: () {},
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: const Text(
+                  "Sign up here",
+                ))
+          ]),
+
+          const Divider(
+            height: 18.0,
+            color: Colors.white,
+          ),
+
+          //Boton de skip
+          Column(children: [
+            ElevatedButton(
+                onPressed: () {
+                  //Navegacion hacia el screen dashboard
+                  Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const ScreenDashboard()));
+                },
+                style: ElevatedButton.styleFrom(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(20.0),
+                  ),
+                ),
+                child: const Text(
+                  "Maybe later",
+                ))
+          ]),
+        ],
+      ),
+    );
+  }
+}
